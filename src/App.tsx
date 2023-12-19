890bb96b8c5211c9ea719a950bf85dc1e1c30a51
mi6u3l <miguel.bgomes@gmail.com>
2023-12-19 15:42:00 +0000

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <iframe src="https://html5.gamemonetize.co/vnelcgo6ketfnul27cvlwr54ij494111/" width="1280" height="720" scrolling="none" frameborder="0"></iframe>
+    </>
+  )
+}
+
+export default App
