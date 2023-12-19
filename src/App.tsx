1df5feb25e4a93199f0f5ff01183ce5a4b3de2a0
mi6u3l <miguel.bgomes@gmail.com>
2023-12-19 15:49:25 +0000

    removes frameborder

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <>
+     <h1>Bazinka</h1>
      <iframe src="https://html5.gamemonetize.co/vnelcgo6ketfnul27cvlwr54ij494111/" width="1280" height="720" scrolling="none"></iframe>
     </>
   )
