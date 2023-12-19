287229d6a9242f5947913451fc422f623205fd6e
mi6u3l <miguel.bgomes@gmail.com>
2023-12-19 15:47:00 +0000

    removes frameborder

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-     <iframe src="https://html5.gamemonetize.co/vnelcgo6ketfnul27cvlwr54ij494111/" width="1280" height="720" scrolling="none" frameborder="0"></iframe>
+     <iframe src="https://html5.gamemonetize.co/vnelcgo6ketfnul27cvlwr54ij494111/" width="1280" height="720" scrolling="none"></iframe>
     </>
   )
 }
