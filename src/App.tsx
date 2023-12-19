573b23863ac53d4c039f90a9c852cbec8daed3c2
mi6u3l <miguel.bgomes@gmail.com>
2023-12-19 18:28:04 +0000

    favico, adds and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import './App.css'
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-
-
   return (
     <>
-     <h1>Bazinka</h1>
-     <iframe src="https://html5.gamemonetize.co/vnelcgo6ketfnul27cvlwr54ij494111/" width="1280" height="720" scrolling="none"></iframe>
+      <img src={logo} style={{ width: "16rem", margin: "1rem" }} />
+      <iframe
+        src="https://html5.gamemonetize.co/vnelcgo6ketfnul27cvlwr54ij494111/"
+        width="1000"
+        height="560"
+        scrolling="none"
+      ></iframe>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
